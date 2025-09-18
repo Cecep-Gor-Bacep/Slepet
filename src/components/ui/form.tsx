6363577fb9193d476f3a14bd07f9e1b86815fea7
YOSAONSITE <2301010184@universitasbumigra.ac.id>
2025-09-19 02:22:12 +0800

    pumal add logo

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { LucideSend } from "lucide-react"
+
 
 export function InputWithButton() {
   return (
     <div className="flex w-full max-w-sm items-center gap-2">
       <Input type="email" placeholder="Nama Lengkap" />
-      <Button type="submit" variant="outline" className="cursor-none">
-        Kirim
-      </Button>
+        <Button className="cursor-none" type="submit" variant={"outline"}>
+          Submit 
+          <LucideSend/>
+        </Button>
     </div>
   )
 }
